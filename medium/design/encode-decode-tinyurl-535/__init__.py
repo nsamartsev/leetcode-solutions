a467e5e2993a6f102251b3aac4ccdebedab35ff3
nsmrtzv <samartsevnikita@gmail.com>
2023-07-20 12:24:38 +0200

    Designed tinyUrl (535)

---
+++ medium/design/encode-decode-tinyurl-535/__init__.py
@@ -0,0 +1,24 @@
+
+class Codec:
+    storage = dict()
+    prefix = "https://short.ru/"
+
+    def encode(self, longUrl: str) -> str:
+        short = self.prefix + str(hash(longUrl))
+        self.storage[short] = longUrl
+        return short
+        
+
+    def decode(self, shortUrl: str) -> str:
+        return self.storage[shortUrl]
+
+
+def main():
+    codec = Codec()
+    
+    assert codec.decode(codec.encode("https://leetcode.com/problems/design-tinyurl")) == "https://leetcode.com/problems/design-tinyurl"
+    
+    print("tests ok")
+
+if __name__ == "__main__":
+    main()
